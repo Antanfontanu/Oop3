b4011f2011b881b11c30009a7426733aa4018a68
Antanfontanu <bblazeviciute@gmail.com>
2025-11-21 23:59:49 +0200

    Pridėti testai prie Cmake

--- main/run.bat
+++ main/run.bat
@@ -1,8 +1,17 @@
+@echo off
+
 if not exist build mkdir build
 cd build
+
 cmake ..
 cmake --build . --config Release
-cd ..
-cd build
+
+echo.
+echo Paleidziami testai...
+.\tests.exe
+
+echo.
+echo Paleidziama programa...
 .\Antanfontanu.exe
+
 pause
